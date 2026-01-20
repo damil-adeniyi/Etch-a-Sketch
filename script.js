032e74a7bee9dfce85fdfafaebd77e60c0c680ba
Damil Adeniyi <damiladeniyi301@gmail.com>
2026-01-20 07:24:46 -0500

    add hover effect on each item

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 const container =  document.querySelector('.container')
+let gridItem = document.createElement('div');
 
 let gridItems = 16;
 let gridSquare = gridItems * gridItems;
 
 function createGrid() {
-    for(let i = 0; i < gridSquare; i++ ){
+    for(let i = 0; i < gridSquare; i++ ) {
+
         let gridItem = document.createElement('div');
 
         gridItem.classList.add('grid-item')
 
-        gridItem.innerText = i + 1;
+        // gridItem.innerText = i + 1;
         
         container.appendChild(gridItem);
 
 
     }
+
 }
 
-createGrid();
+createGrid()
+
+let btn =  document.querySelector('button');
+
+btn.addEventListener('click' () => {
+    
+})
+
+
